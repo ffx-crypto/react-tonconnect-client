04d3fb3abec452809c360a5489b898990d9ea20d
gilert <crankyfox@protonmail.com>
2024-10-19 21:18:01 +0200

    move code from Sale to JSalesInput component

--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTonWallet, useTonConnectUI } from "@tonconnect/ui-react";
-import JSalesInput from "../components/JSalesInput/JSaleseInput";
+import JSalesInput from "../components/JSalesInput/JSalesInput";
 import {
   Address,
   beginCell,
@@ -11,49 +11,50 @@ import {
 } from "@ton/core";
 import { useTonClient } from "../hooks/useTonClient";
 
-const waitForTransaction = async (options, client) => {
-  const { hash, refetchInterval = 1000, refetchLimit, address } = options;
+// const waitForTransaction = async (options, client) => {
+//   const { hash, refetchInterval = 5000, refetchLimit, address } = options;
 
-  return new Promise((resolve) => {
-    let refetches = 0;
-    const walletAddress = Address.parse(address);
-    const interval = setInterval(async () => {
-      refetches += 1;
+//   return new Promise((resolve) => {
+//     let refetches = 0;
+//     const walletAddress = Address.parse(address);
+//     const interval = setInterval(async () => {
+//       refetches += 1;
 
-      console.log("waiting transaction...");
-      const state = await client.getContractState(walletAddress);
-      if (!state || !state.lastTransaction) {
-        clearInterval(interval);
-        resolve(null);
-        return;
-      }
-      const lastLt = state.lastTransaction.lt;
-      const lastHash = state.lastTransaction.hash;
-      const lastTx = await client.getTransaction(
-        walletAddress,
-        lastLt,
-        lastHash
-      );
+//       console.log("waiting transaction...");
+//       const state = await client.getContractState(walletAddress);
+//       if (!state || !state.lastTransaction) {
+//         clearInterval(interval);
+//         resolve(null);
+//         return;
+//       }
+//       const lastLt = state.lastTransaction.lt;
+//       const lastHash = state.lastTransaction.hash;
+//       // console.log('state, lastLt, lastHash ', state, lastLt, lastHash);
+//       const lastTx = await client.getTransaction(
+//         walletAddress,
+//         lastLt,
+//         lastHash
+//       );
 
-      if (lastTx && lastTx.inMessage) {
-        const msgCell = beginCell()
-          .store(storeMessage(lastTx.inMessage))
-          .endCell();
+//       if (lastTx && lastTx.inMessage) {
+//         const msgCell = beginCell()
+//           .store(storeMessage(lastTx.inMessage))
+//           .endCell();
 
-        const inMsgHash = msgCell.hash().toString("base64");
-        console.log("InMsgHash", inMsgHash);
-        if (inMsgHash === hash) {
-          clearInterval(interval);
-          resolve(lastTx);
-        }
-      }
-      if (refetchLimit && refetches >= refetchLimit) {
-        clearInterval(interval);
-        resolve(null);
-      }
-    }, refetchInterval);
-  });
-};
+//         const inMsgHash = msgCell.hash().toString("base64");
+//         console.log("InMsgHash", inMsgHash);
+//         if (inMsgHash === hash) {
+//           clearInterval(interval);
+//           resolve(lastTx);
+//         }
+//       }
+//       if (refetchLimit && refetches >= refetchLimit) {
+//         clearInterval(interval);
+//         resolve(null);
+//       }
+//     }, refetchInterval);
+//   });
+// };
 
 const Sale = () => {
   const wallet = useTonWallet();
@@ -63,48 +64,7 @@ const Sale = () => {
   const { client } = useTonClient();
   const [finalizedTx, setFinalizedTx] = useState(null);
 
-  const handleSendTon = async (tonAmount) => {
-    tonAmount = tonAmount.toString();
-    try {
-      const tx = {
-        // The transaction is valid for 10 minutes from now, in unix epoch seconds.
-        validUntil: Math.floor(Date.now() / 1000) + 600,
-        messages: [
-          {
-            // The receiver's address.
-            address: import.meta.env.VITE_MINTER_ADMIN_ADDRESS,
-            // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
-            amount: tonAmount, // 0.0005 TON
-          },
-        ],
-      };
-      console.log('tx ',tx);
-      const result = await tonConnectUi.sendTransaction(tx);
-      
-      setLoading(true);
-      const hash = Cell.fromBase64(result.boc).hash().toString("base64");
-
-      const message = loadMessage(Cell.fromBase64(result.boc).asSlice());
-      console.log("Message:", message.body.hash().toString("hex"));
-      setMsgHash(hash);
-      console.log('address ', tonConnectUi.account?.address);
-
-      if (client) {
-        const txFinalized = await waitForTransaction(
-          {
-            address: tonConnectUi.account?.address ?? "",
-            hash: hash,
-          },
-          client
-        );
-        setFinalizedTx(txFinalized);
-      }
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setLoading(false);
-    } 
-  };
+  
 
   return (
     <div>
@@ -112,27 +72,7 @@ const Sale = () => {
         Welcome to the Sales Page
       </h1>
 
-      {wallet ? (
-        <JSalesInput loading={loading} onClick={handleSendTon} />
-      ) : (
-        <div className=" flex flex-col h-32 justify-center">
-          <button
-            onClick={() => tonConnectUi.openModal()}
-            className="p-4 bg-sky-500 w-content rounded-full text-white font-semibold mx-auto"
-          >
-            Connect wallet to send the transaction
-          </button>
-          {/* <h2 className="text-lg text-center text-2xl font-semibold">
-            You should connect your wallet first to be able to buy jettons.
-          </h2> */}
-          {/* <button
-            onClick={() => tonConnectUI.openModal()}
-            className="p-4 bg-sky-500 w-[12em] rounded-full text-white font-semibold mx-auto"
-          >
-            Connect Wallet
-          </button> */}
-        </div>
-      )}
+      <JSalesInput />
     </div>
   );
 };
