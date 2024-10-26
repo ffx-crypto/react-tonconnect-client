01b934c30eba50ba78d4b72298888aa94ef2ee29
gilert <crankyfox@protonmail.com>
2024-10-26 13:21:13 +0200

    add layout elements in the JSalesInput component

--- src/components/JSalesInput/JSalesInput.jsx
+++ src/components/JSalesInput/JSalesInput.jsx
@@ -1,53 +1,20 @@
-import { useCallback, useState } from "react";
-import {
-  
-  useTonConnectUI,
-  useTonWallet,
-} from "@tonconnect/ui-react";
+import { useCallback, useEffect, useState } from "react";
+import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 import {
   Address,
   beginCell,
   Cell,
   loadMessage,
   storeMessage,
+  toNano,
 } from "@ton/core";
 import { useTonClient } from "../../hooks/useTonClient";
-
 
 // In this example, we are using a predefined smart contract state initialization (`stateInit`)
 // to interact with an "EchoContract". This contract is designed to send the value back to the sender,
 // serving as a testing tool to prevent users from accidentally spending money.
-const defaultTx= {
-  // The transaction is valid for 10 minutes from now, in unix epoch seconds.
-  validUntil: Math.floor(Date.now() / 1000) + 600,
-  messages: [
-    {
-      // The receiver's address.
-      address: "UQDrwH5ivaR3Vf6C7Grq_sbhcXZnrAo9ZZaz0jn6GEI3gziW",
-      // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
-      amount: "1555440", // 0.0005 TON
-      // (optional) State initialization in boc base64 format.
-      // stateInit:
-      //   "te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==",
-      // (optional) Payload in boc base64 format.
-      // payload: "te6ccsEBAQEADAAMABQAAAAASGVsbG8hCaTc/g==",
-    },
 
-    // Uncomment the following message to send two messages in one transaction.
-    /*
-    {
-      // Note: Funds sent to this address will not be returned back to the sender.
-      address: 'UQAuz15H1ZHrZ_psVrAra7HealMIVeFq0wguqlmFno1f3B-m',
-      amount: toNano('0.01').toString(),
-    }
-    */
-  ],
-};
-
-
-
-const waitForTransaction = async (
-  options, client) => {
+const waitForTransaction = async (options, client) => {
   const { hash, refetchInterval = 1000, refetchLimit, address } = options;
 
   return new Promise((resolve) => {
@@ -58,7 +25,7 @@ const waitForTransaction = async (
 
       console.log("waiting transaction...");
       const state = await client.getContractState(walletAddress);
-      console.log('state ', state);
+      console.log("state ", state);
       if (!state || !state.lastTransaction) {
         clearInterval(interval);
         resolve(null);
@@ -78,7 +45,7 @@ const waitForTransaction = async (
           .endCell();
 
         const inMsgHash = msgCell.hash().toString("base64");
-        console.log("InMsgHash", inMsgHash);
+        console.log("InMsgHash hex", msgCell.hash().toString('hex'));
         if (inMsgHash === hash) {
           clearInterval(interval);
           resolve(lastTx);
@@ -93,42 +60,68 @@ const waitForTransaction = async (
 };
 
 export default function JSalesInput() {
-  const [tx, setTx] = useState(defaultTx);
+  const [jettonAmount, setJettonAmount] = useState(0);
+  const [jettonPrice, setJettonPrice] = useState(0);
+  // const defaultTx = {
+  //   // The transaction is valid for 10 minutes from now, in unix epoch seconds.
+  //   validUntil: Math.floor(Date.now() / 1000) + 600,
+  //   messages: [
+  //     {
+  //       // The receiver's address.
+  //       address: "UQDrwH5ivaR3Vf6C7Grq_sbhcXZnrAo9ZZaz0jn6GEI3gziW",
+  //       // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
+  //       amount: jettonPrice, //
+  //     },
+  //   ],
+  // };
+  // const [tx, setTx] = useState(defaultTx);
   const [finalizedTx, setFinalizedTx] = useState(null);
   const [msgHash, setMsgHash] = useState("");
   const [loading, setLoading] = useState(false);
   const { client } = useTonClient();
-
+  const oneJettonPrice = import.meta.env.VITE_JETTON_PRICE;
+  const minterAdminAddr = import.meta.env.VITE_MINTER_ADMIN_ADDRESS;
   const wallet = useTonWallet();
 
   const [tonConnectUi] = useTonConnectUI();
 
   // const { waitForTransaction } = useWaitForTransaction(client!!);
 
-  const onChange = useCallback((value) => {
-    setTx(value.updated_src);
-  }, []);
+  // const onChange = useCallback((value) => {
+  //   setTx(value.updated_src);
+  // }, []);
+
+  useEffect(() => {
+    const totalPrice = jettonAmount * oneJettonPrice;
+    setJettonPrice(parseFloat(totalPrice.toFixed(3)).toString());
+  }, [jettonAmount]);
 
   const handleSendTons = async () => {
-            
+    const tx = {
+      validUntil: Math.floor(Date.now() / 1000) + 600,
+      messages: [
+        {
+          address: import.meta.env.VITE_MINTER_ADMIN_ADDRESS,
+          amount: toNano(jettonPrice).toString(),
+        },
+      ],
+    };
+    console.log("tx ", tx);
     try {
       const result = await tonConnectUi.sendTransaction(tx);
       setLoading(true);
-      const hash = Cell.fromBase64(result.boc)
-        .hash()
-        .toString("base64");
+      const hash = Cell.fromBase64(result.boc).hash().toString("base64");
 
-      const message = loadMessage(
-        Cell.fromBase64(result.boc).asSlice()
-      );
-      console.log('hash ', hash);
+      const message = loadMessage(Cell.fromBase64(result.boc).asSlice());
+      console.log("hash ", hash);
       console.log("Message:", message.body.hash().toString("hex"));
       setMsgHash(hash);
-      console.log('address ', tonConnectUi.account?.address);
+      console.log("address ", tonConnectUi.account?.address);
       if (client) {
         const txFinalized = await waitForTransaction(
           {
             address: tonConnectUi.account?.address ?? "",
+            refetchInterval: 3000,
             hash: hash,
           },
           client
@@ -140,28 +133,77 @@ export default function JSalesInput() {
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   return (
-    <div className="send-tx-form">
-      <h3>Configure and send transaction</h3>
+    <div className="max-w-sm mx-auto bg-white rounded-lg shadow-lg p-6">
+      {/* Image and Address */}
+      <div className="flex flex-col items-center mb-6">
+        <img
+          src="token-ffx-logo.webp"
+          alt="Jetton logo"
+          className="w-24 h-24 rounded-full object-cover mb-2"
+        />
+        <p className="text-gray-700 text-lg font-semibold text-center">
+          {minterAdminAddr}
+        </p>
+      </div>
 
-      
+      {/* Input for Jettons */}
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="amount"
+        ></label>
+        <input
+          type="number"
+          id="amount"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={jettonAmount}
+          onChange={(e) => setJettonAmount(parseFloat(e.target.value))}
+          placeholder="Enter amount"
+          min="0"
+        />
+      </div>
 
       {wallet ? (
         <button
           disabled={loading}
           onClick={handleSendTons}
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-          {loading ? "Loading..." : "Send transaction"}
+          {loading ? (
+            "Loading..."
+          ) : (
+            <>
+              Send{" "}
+              <span className="text-yellow-300 font-bold">{jettonPrice}</span>{" "}
+              ton
+            </>
+          )}
         </button>
       ) : (
-        <button onClick={() => tonConnectUi.openModal()}>
+        <button
+          onClick={() => tonConnectUi.openModal()}
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
           Connect wallet to send the transaction
         </button>
       )}
-      <div>Sending Tx Message Hash: {msgHash}</div>
-      <div>Sending Tx Hash: {finalizedTx?.hash().toString('hex')}</div>
+      {msgHash ? (
+        <>
+          <div className="break-words mt-4"><span className="text-gray-400 font-bold">Sending Tx Message Hash:</span> {msgHash}</div>
+          {finalizedTx ? (
+            <>
+              <div className="break-words mt-2"><span className="text-gray-400 font-bold">Sending Tx Hash:</span> {finalizedTx?.hash().toString("hex")}</div>
+            </>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
