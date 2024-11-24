5d2c7766423523646b10a770269d8585d85d791e
gilert <crankyfox@protonmail.com>
2024-11-24 14:27:49 +0100

    comment loadMessage() that provoked errors

--- src/hooks/useTonTransaction.js
+++ src/hooks/useTonTransaction.js
@@ -8,9 +8,11 @@ import {
   toNano,
 } from "@ton/core";
 import { useTonConnectUI } from "@tonconnect/ui-react";
+import { UserRejectsError } from "@tonconnect/sdk";
 
 export const useTonTransaction = () => {
   const [finalizedTx, setFinalizedTx] = useState(null);
+  const [lastTxHash, setLastTxHash] = useState(null);
   const [msgHash, setMsgHash] = useState("");
   const [loading, setLoading] = useState(false);
   const [tonConnectUi] = useTonConnectUI();
@@ -27,7 +29,7 @@ export const useTonTransaction = () => {
 
         console.log("waiting transaction...");
         const state = await client.getContractState(walletAddress);
-        console.log("state ", state);
+        console.log("state ", state); // should i use state.lastTransaction.hash instead of finalizedTx ?
         if (!state || !state.lastTransaction) {
           clearInterval(interval);
           resolve(null);
@@ -47,7 +49,11 @@ export const useTonTransaction = () => {
             .endCell();
 
           const inMsgHash = msgCell.hash().toString("base64");
-          console.log("InMsgHash hex", msgCell.hash().toString("hex"));
+          console.log(
+            "InMsgHash, InMsgHash hex",
+            inMsgHash,
+            msgCell.hash().toString("hex")
+          );
           if (inMsgHash === hash) {
             clearInterval(interval);
             resolve(lastTx);
@@ -70,40 +76,45 @@ export const useTonTransaction = () => {
           amount: toNano(tonAmount).toString(),
         },
       ],
-    }; 
-    try { 
+    };
+    try {
       const result = await tonConnectUi.sendTransaction(tx);
       setLoading(true);
       const hash = Cell.fromBase64(result.boc).hash().toString("base64");
 
-      const message = loadMessage(Cell.fromBase64(result.boc).asSlice());
+      // const message = loadMessage(Cell.fromBase64(result.boc).asSlice());
       console.log("hash ", hash);
-      console.log("Message:", message.body.hash().toString("hex"));
+      // console.log("Message:", message.body.hash().toString("hex"));
       setMsgHash(hash);
       console.log("address ", tonConnectUi.account?.address);
       if (client) {
         const txFinalized = await waitForTransaction(
           {
             address: tonConnectUi.account?.address ?? "",
-            refetchInterval: 3000,
-            refetchLimit: 10,
+            refetchInterval: 5000,
+            refetchLimit: 7,
             hash: hash,
           },
           client
-        ); 
+        );
         if (txFinalized) {
-          setFinalizedTx(txFinalized); 
-        await sendTxHash(hash, comment);
+          setFinalizedTx(txFinalized);
+          const txHash = txFinalized.hash().toString('base64');
+          await sendTxHash(hash, txHash, comment);
         }
       }
     } catch (e) {
-      console.error(e);
+      if (e instanceof UserRejectsError) {
+        console.error(e);
+      } else {
+        setResponseError(`Error during sending transaction: ${e}`);
+      }
     } finally {
       setLoading(false);
     }
   };
 
-  const sendTxHash = async (hash, comment) => {
+  const sendTxHash = async (hash, txHash, comment) => {
     const api_url = import.meta.env.VITE_API_BASE_URL;
 
     try {
@@ -112,21 +123,25 @@ export const useTonTransaction = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ msgHash: hash, comment: comment }),
+        body: JSON.stringify({
+          msgHash: hash,
+          txHash: txHash,
+          comment: comment,
+        }),
       });
 
       if (response.status === 404) {
-        setResponseError("Transaction not found");
+        setResponseError("Transaction not cofirmed. Contact administrator.");
       } else if (response.status === 500) {
         setResponseError("Server error. Contact administrator.");
       } else if (response.ok) {
         const data = await response.json();
         setResponseMessage(data.message); // assuming { message: 'Transaction successful' }
       } else {
-        setResponseError("Unexpected error occurred");
+        setResponseError("Unexpected error occurred. Contact administrator.");
       }
     } catch (error) {
-      setResponseError("Failed to connect to the server");
+      setResponseError("Failed to connect to the server. Contact administrator.");
       console.error("Request error:", error);
     }
   };
@@ -137,6 +152,8 @@ export const useTonTransaction = () => {
     msgHash,
     finalizedTx,
     responseMsg,
-    responseError
+    responseError,
+    setResponseError,
+    setResponseMessage,
   };
 };
