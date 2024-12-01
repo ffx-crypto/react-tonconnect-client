7f83baf528cbc4e3176a50568a15c08701294db3
gilert <crankyfox@protonmail.com>
2024-12-01 19:52:41 +0100

    add warning message on the Sale page

--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import JSalesInput from "../components/JSalesInput/JSalesInput";
+import { MdWarning } from "react-icons/md";
 
 const Sale = () => {
   const oneJettonPrice = import.meta.env.VITE_JETTON_PRICE;
@@ -9,13 +10,14 @@ const Sale = () => {
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-8 text-center my-8">
         Sale Page
       </h1>
-      <div className="mb-8 space-y-4">
+      <div className="mb-12 space-y-4">
         <p className="text-base sm:text-lg md:text-xl text-gray-700 text-justify">
           On this page you can exchange <b>ton</b> tokens on <b>FFX jettons</b>.
         </p>
         <p className="text-base sm:text-lg md:text-xl text-gray-700 text-justify">
-          The price of 1 jetton - <b>{oneJettonPrice} ton</b>. You will also need to send a
-          minimal forward fees amount equal to - <b>{minForwardAmount} ton</b>, more about this   
+          The price of 1 jetton - <b>{oneJettonPrice} ton</b>. You will also
+          need to send a minimal forward fees amount equal to -{" "}
+          <b>{minForwardAmount} ton</b>, more about this
           <a
             href="https://docs.ton.org/v3/documentation/smart-contracts/transaction-fees/fees"
             target="_blank"
@@ -26,11 +28,22 @@ const Sale = () => {
           </a>
         </p>
         <p className="text-base sm:text-lg md:text-xl text-gray-700 text-justify">
-          You can also send a comment with your purchase, so that admin will be able to read. Unlike a transaction comment that is recorded in the TON blockchain, this comment will be inserted in the database.  
+          You can also send a comment with your purchase, so that admin will be
+          able to read. Unlike a transaction comment that is recorded in the TON
+          blockchain, this comment will be inserted in the database.
         </p>
-        <p className="text-base sm:text-lg md:text-xl text-gray-700 text-justify font-bold">
-          Don't spend more than you are ready to lose. 
-        </p>
+        {/* WARNING */}
+        <div className="flex flex-col text-base sm:text-lg md:text-xl text-gray-700 text-justify  bg-amber-100 p-8 rounded-lg border-l-orange-300 border-l-4 shadow-md">
+          <div className="flex">
+            <MdWarning
+              style={{ marginRight: "8px", height: "2rem", width: "2rem" }}
+              size={24}
+            />
+            <span className="uppercase font-bold">Caution</span>
+          </div>
+          <p>Currently only the desktop version works.</p>
+          <p>Don't spend on crypto more than you are ready to lose.</p>
+        </div>
       </div>
       <JSalesInput />
       <div className="mb-8"></div>
